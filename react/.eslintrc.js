598b9f5b767681027162e6befcd22feb1cbf794a
John Carmichael <jc@stackeducation.com>
2020-03-24 18:52:50 -0400

    Moved Node rules into node folder and created React rules in react folder

---
+++ react/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": { "es6": true, "node": true, "browser": true },
+  "extends": "eslint:recommended",
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaFeatures": { "jsx": true },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": ["react"],
+  "rules": {
+    "camelcase": 0,
+    "indent": [ "error", 2, { "SwitchCase": 1 }],
+    "jsx-a11y/label-has-for": 0,
+    "linebreak-style": ["error", "unix"],
+    "max-len": ["error", { "code": 120, "ignoreComments": true, "ignoreTrailingComments": true }],
+    "object-curly-newline": ["error", { "multiline": true, "minProperties": 5, "consistent": true }],
+    "react/prop-types": 0,
+    "semi": ["error", "never"],
+    "quotes": ["error", "single"],
+  }
+};
---
+++ react/playground.js
@@ -0,0 +1,47 @@
+const goodArray = [1, 2, 3]
+const badArrayCommaSpacing = [1,2,3]
+const badArraySpacing = [ 1, 2, 3 ]
+
+const goodArrayNewLine = [
+  1, 2, 3, 4, 5,
+]
+const badArrayNewLine = [
+  1, 2, 3,
+  4, 5,
+]
+
+const goodArrow = (a) => a
+const badArrowSpaceBefore = ()=> {}
+const badArrowSpaceAfter = () =>{}
+
+// goodBlockSpacing
+let a = 'a'
+
+if (a === 'a') { a = 1 }
+
+// badBlockSpacing
+if (a === 'a') {a = 1}
+
+const foo = { baz: '' }
+const badDotPlacement = foo.
+  baz
+const goodDotPlacement = foo
+  .baz
+
+const badObjectNewLine = { foo: 1, bar: 2, baz: 3, bang: 4, bong: 5 }
+const goodObjectNewLine = { foo: 1, bar: 2, baz: 3, bang: 4 }
+
+module.exports = {
+  goodArray,
+  badArrayCommaSpacing,
+  badArraySpacing,
+  goodArrayNewLine,
+  badArrayNewLine,
+  goodArrow,
+  badArrowSpaceBefore,
+  badArrowSpaceAfter,
+  badDotPlacement,
+  goodDotPlacement,
+  badObjectNewLine,
+  goodObjectNewLine,
+}
